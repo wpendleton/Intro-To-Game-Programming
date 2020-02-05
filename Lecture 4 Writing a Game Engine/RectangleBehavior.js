a37186f749bac267f2da794d07219cbf2fca1ec2
UNO User <unouser@unomaha.edu>
2020-02-05 14:31:43 -0600

    Renamed lecture 4 folder

---
+++ Lecture 4 Writing a Game Engine/RectangleBehavior.js
@@ -0,0 +1,9 @@
+import Behavior from "./Behavior.js"
+
+class RectanlgeBehavior extends Behavior{
+    /* Set the inital rotation to 0 */
+
+    /* rotate slowly with time */
+}
+
+export default RectanlgeBehavior;
